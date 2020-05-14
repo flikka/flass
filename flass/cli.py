d8a6b1c5ce4e9da01e102ee9379512d3859d3119
Kristian Flikka <kristian.flikka@gmail.com>
2020-05-14 09:33:47 +0200

    Add lime lime (#6)
    * Attempt at using Lime
    
    * Lime option and random images

--- flass/cli.py
+++ flass/cli.py
@@ -1,11 +1,17 @@
 import logging
+from pprint import pformat
+import random
 import sys
-import os
 
 import click
-import numpy as np
+from lime import lime_image
+from lime.wrappers.scikit_image import SegmentationAlgorithm
+import matplotlib.pyplot as plt
 import mlflow
+import numpy as np
+from skimage.segmentation import mark_boundaries
 from sklearn.metrics import roc_auc_score, classification_report
+
 from flass.model import train, get_data, plot_incorrect
 
 logging.basicConfig(
@@ -23,14 +29,13 @@ logger = logging.getLogger(__name__)
 @click.option("--dataset", required=True, help="Choose between {fashion, mnist}")
 @click.option("--model-type", required=False, default="kerasconv")
 @click.option("--subset", required=False, default=-1)
+@click.option("--lime/--no-lime", default=False)
 @click.command()
-def flass(plot, batch_size, epochs, dataset, model_type, subset):
+def flass(plot, batch_size, epochs, dataset, model_type, subset, lime):
     logger.info("Obtaining data")
     data, class_names = get_data(dataset, subset)
 
     (x_train, y_train), (x_test, y_test) = data
-    x_train = np.expand_dims(x_train, -1)
-    x_test = np.expand_dims(x_test, -1)
 
     with mlflow.start_run(run_name=dataset):
         mlflow.log_param("batch_size", batch_size)
@@ -44,6 +49,12 @@ def flass(plot, batch_size, epochs, dataset, model_type, subset):
             epochs=epochs,
             model_type=model_type,
         )
+
+        if lime:
+            # Do a LIME
+            samples = random.sample(range(0, len(x_test)), 10)
+            for i in samples:
+                limeify(x_test[i], trained_pipeline, class_names)
 
         predicted_y_probabilities = trained_pipeline.predict(x_test)
         roc_auc = roc_auc_score(y_test, predicted_y_probabilities, multi_class="ovr")
@@ -71,6 +82,37 @@ def flass(plot, batch_size, epochs, dataset, model_type, subset):
 
     if plot:
         plot_incorrect(x_test, y_test, y_predicted, class_names)
+
+
+def limeify(image_to_explain, trained_pipeline, class_names):
+    logger.info("Start a LIME explanation")
+    lime_image_probabilities = trained_pipeline.predict(np.array([image_to_explain]))[0]
+    image_probabilities = tuple(zip(class_names, lime_image_probabilities))
+    logger.info(
+        "Models predicted probabilities for image:\n" + pformat(image_probabilities)
+    )
+    plt.imshow(image_to_explain)
+    plt.show()
+    explainer = lime_image.LimeImageExplainer()
+    segmenter = SegmentationAlgorithm(
+        "quickshift", kernel_size=1, max_dist=200, ratio=0.2
+    )
+    explanation = explainer.explain_instance(
+        image_to_explain,
+        trained_pipeline.predict,
+        top_labels=10,
+        num_samples=1000,
+        segmentation_fn=segmenter,
+    )
+    logger.info("Done with a LIME")
+    temp, mask = explanation.get_image_and_mask(
+        explanation.top_labels[0],
+        positive_only=False,
+        num_features=5,
+        hide_rest=False,
+    )
+    plt.imshow(mark_boundaries(temp, mask))
+    plt.show()
 
 
 if __name__ == "__main__":
